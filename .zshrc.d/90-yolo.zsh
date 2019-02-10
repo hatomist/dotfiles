3c6c8501bebe8f2b0181a0fac5f9eec021fdd566
Sergey Shatunov <me@prok.pw>
2019-02-10 16:06:18 +0700

    grrrr

--- .zshrc.d/90-yolo.zsh
+++ .zshrc.d/90-yolo.zsh
@@ -2,3 +2,15 @@ alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc'
 alias zplugrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zplugrc'
 alias yolo='git commit -m "$(curl -s whatthecommit.com/index.txt)"'
 
+[[ "$ZDOTDIR" == "$HOME" ]] || function rc {
+  case "$1" in
+    yolo)
+      git -C "$ZDOTDIR" add "$ZDOTDIR"
+      git -C "$ZDOTDIR" commit -m "$(curl -s whatthecommit.com/index.txt)" "${@:2}"
+      git -C "$ZDOTDIR" push
+      ;;
+    *)
+      git -C "$ZDOTDIR" "${@:1}"
+      ;;
+  esac
+}

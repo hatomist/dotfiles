91b408ecf7036837ac53285f672e71770a834dc8
Sergey Shatunov <me@prok.pw>
2019-02-10 15:20:50 +0700

    Revert "fuckup".

--- .zshrc.d/10-zplug.zsh
+++ .zshrc.d/10-zplug.zsh
@@ -1,10 +1,15 @@
 #!/usr/bin/env zsh
 ZPLUG_HOME="${ZDOTDIR:-$HOME}/.zplug"
+ZPLUG_LOADFILE="${ZDOTDIR:-$HOME}/.zplugrc"
 
-[[ -d "$ZPLUG_HOME" ]] || git clone --quiet https://github.com/zplug/zplug.git "$ZPLUG_HOME"
+[[ -d "$ZPLUG_HOME" ]] || {
+  git clone --quiet https://github.com/zplug/zplug.git "$ZPLUG_HOME"
+
+  # Fix for long-waited first loading, remove it when merged upstream
+  git -C "$ZPLUG_HOME" apply <(curl -s https://github.com/crhg/zplug/commit/5632b8ad22dbcaa4b6ea485bb852684125157f5f.patch)
+}
 
 source "$ZPLUG_HOME/init.zsh"
-source "${ZDOTDIR:-$HOME}/.zplugrc"
 
 if ! zplug check; then
     zplug install
--- .zshrc.d/20-zkbd.zsh
+++ .zshrc.d/20-zkbd.zsh
@@ -1,16 +1,24 @@
 #!/usr/bin/env zsh
 [[ -e "${ZDOTDIR:-$HOME}/.zkbd/$TERM-$VENDOR-$OSTYPE" ]] && source "${ZDOTDIR:-$HOME}/.zkbd/$TERM-$VENDOR-$OSTYPE"
+[[ -e "${ZDOTDIR:-$HOME}/.zkbd/$TERM-${${DISPLAY:t}:-$VENDOR-$OSTYPE}" ]] && source "${ZDOTDIR:-$HOME}/.zkbd/$TERM-${${DISPLAY:t}:-$VENDOR-$OSTYPE}"
+
+_zkbd_missing=false
 
 function _bind_zkbd_key() {
   local _key="${key[$1]}"
-  [[ -n $_key ]] && bindkey "$_key" $2
+  [[ -n $_key ]] && bindkey "$_key" $2 || _zkbd_missing=true
 }
 
-_bind_zkbd_key "${key[Left]}"   backward-char
-_bind_zkbd_key "${key[Right]}"  forward-char
-_bind_zkbd_key "${key[Home]}"  beginning-of-line
-_bind_zkbd_key "${key[End]}"    end-of-line
-_bind_zkbd_key "${key[Delete]}" delete-char
+_bind_zkbd_key Left   backward-char
+_bind_zkbd_key Right  forward-char
+_bind_zkbd_key Up     up-line-or-history
+_bind_zkbd_key Down   down-line-or-history
+_bind_zkbd_key Home   beginning-of-line
+_bind_zkbd_key End    end-of-line
+_bind_zkbd_key Delete delete-char
 
 unfunction _bind_zkbd_key
 
+[[ "$_zkbd_missing" == "true" ]] && echo " ### Please generate zkbd mapping with 'autoload zkbd && zkbd' ###"
+
+unset _zkbd_missing

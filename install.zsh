69e0575b96846e89b3e4d077cdab272c3ec6691b
Sergey Shatunov <me@prok.pw>
2019-02-10 14:38:55 +0700

    Fix issue in headless installer

--- install.zsh
+++ install.zsh
@@ -10,8 +10,13 @@ function _install_symlink() {
 _install_method=_install_symlink
 
 function _install_dotfiles() {
+  local REPO="https://gitlab.com/Prototik/dotfiles.git"
   local TARGET_DIR=${ZDOTDIR:P}
-  local DOTFILES_DIR=${0:P:h}
+  local DOTFILES_DIR
+  case "$0" in
+    /*) DOTFILES_DIR="${0:P:h}"      ;;
+    *)  DOTFILES_DIR="$PWD/${0:P:h}" ;;
+  esac
   local REPO="https://gitlab.com/Prototik/dotfiles.git"
 
   if [[ "$TARGET_DIR" == "$DOTFILES_DIR" ]]; then

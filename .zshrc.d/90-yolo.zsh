e4e132ea172e5fc176a6f71bb7a91de5e76c1b0a
Sergey Shatunov <me@prok.pw>
2019-02-11 19:40:35 +0700

    bug fix

--- .zshrc.d/90-yolo.zsh
+++ .zshrc.d/90-yolo.zsh
@@ -2,7 +2,7 @@ alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc'
 alias zplugrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zplugrc'
 alias yolo='git commit -m "$(curl -s whatthecommit.com/index.txt)"'
 
-[[ "$ZDOTDIR" == "$HOME" ]] || function rc {
+[[ "${ZDOTDIR:-$HOME}" != "$HOME" ]] && function rc {
   case "$1" in
     cd)
       cd "$ZDOTDIR"

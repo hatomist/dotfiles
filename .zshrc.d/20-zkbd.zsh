55150430b8a687215e23e4bd37e324db4e0aa740
Sergey Shatunov <me@prok.pw>
2019-02-13 22:26:50 +0700

    For the sake of my sanity, just ignore this...

--- .zshrc.d/20-zkbd.zsh
+++ .zshrc.d/20-zkbd.zsh
@@ -1,25 +1,41 @@
 #!/usr/bin/env zsh
-[[ -e "${ZDOTDIR:-$HOME}/.zkbd/$TERM" ]] && source "${ZDOTDIR:-$HOME}/.zkbd/$TERM"
-[[ -e "${ZDOTDIR:-$HOME}/.zkbd/$TERM-$VENDOR" ]] && source "${ZDOTDIR:-$HOME}/.zkbd/$TERM-$VENDOR"
-[[ -e "${ZDOTDIR:-$HOME}/.zkbd/$TERM-$VENDOR-$OSTYPE" ]] && source "${ZDOTDIR:-$HOME}/.zkbd/$TERM-$VENDOR-$OSTYPE"
-[[ -e "${ZDOTDIR:-$HOME}/.zkbd/$TERM-${${DISPLAY:t}:-$VENDOR-$OSTYPE}" ]] && source "${ZDOTDIR:-$HOME}/.zkbd/$TERM-${${DISPLAY:t}:-$VENDOR-$OSTYPE}"
 
-_zkbd_missing=false
+typeset -g -a zkbd_keymaps
+typeset -g -A zkbd_mapping
+typeset -g -A key
 
-function _bind_zkbd_key() {
-  [[ -n ${key[$1]} ]] && bindkey "${key[$1]}" $2 || _zkbd_missing=true
+zkbd_keymaps+=("${ZDOTDIR:-$HOME}/.zkbd/$TERM")
+zkbd_keymaps+=("${ZDOTDIR:-$HOME}/.zkbd/$TERM-$VENDOR")
+zkbd_keymaps+=("${ZDOTDIR:-$HOME}/.zkbd/$TERM-$VENDOR-$OSTYPE")
+zkbd_keymaps+=("${ZDOTDIR:-$HOME}/.zkbd/$TERM-${${DISPLAY:t}:-$VENDOR-$OSTYPE}")
+
+zkbd_mapping[Left]=backward-char
+zkbd_mapping[Right]=forward-char
+zkbd_mapping[Up]=up-line-or-history
+zkbd_mapping[Down]=down-line-or-history
+zkbd_mapping[Home]=beginning-of-line
+zkbd_mapping[End]=end-of-line
+zkbd_mapping[Delete]=delete-char
+
+function __kk::zkbd::load() {
+  [[ -e "$1" ]] && source "$1"
 }
 
-_bind_zkbd_key Left   backward-char
-_bind_zkbd_key Right  forward-char
-_bind_zkbd_key Up     up-line-or-history
-_bind_zkbd_key Down   down-line-or-history
-_bind_zkbd_key Home   beginning-of-line
-_bind_zkbd_key End    end-of-line
-_bind_zkbd_key Delete delete-char
+function __kk::zkbd::bind() {
+  [[ -n ${key[$1]} ]] && bindkey "${key[$1]}" "$2" || [[ -v __kk_zkbd_missing ]] || {
+    echo " ### Please generate zkbd mapping with 'autoload zkbd && zkbd' ###" >&2
+    __kk_zkbd_missing=true
+  }
+}
 
-unfunction _bind_zkbd_key
+for mapping in "${zkbd_keymaps[@]}"; do
+  __kk::zkbd::load "$mapping"
+done
 
-[[ "$_zkbd_missing" == "true" ]] && echo " ### Please generate zkbd mapping with 'autoload zkbd && zkbd' ###"
+unset __kk_zkbd_missing
 
-unset _zkbd_missing
+for k in "${(@k)zkbd_mapping}"; do
+  __kk::zkbd::bind "$k" "$zkbd_mapping[$k]"
+done
+
+unset __kk_zkbd_missing
